38c18b3de96356ca1836fe56011c0e4b3576766a
TD2K <diemtruong2000@gmail.com>
2022-06-12 18:01:09 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Fragment } from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
-    return <div className="App">Hello World!</div>;
+    return (
+        <Router>
+            <Routes>
+                {publicRoutes.map((publicRoute, index) => {
+                    const Page = publicRoute.component;
+
+                    let Layout = DefaultLayout;
+
+                    if (publicRoute.layout) {
+                        Layout = publicRoute.layout;
+                    } else if (publicRoute.layout === null) {
+                        Layout = Fragment;
+                    }
+
+                    return (
+                        <Route
+                            key={index}
+                            path={publicRoute.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
